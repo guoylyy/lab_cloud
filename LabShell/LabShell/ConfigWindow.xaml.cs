03696ce512e731bb4f57dcc956e611bd3010b4e2
PanYan <panyan5617998@163.com>
2014-10-23 14:10:52 +0800

    LabShell 上传
    完成初始界面和配置界面（只是一个壳），完成拍照API（可以按照时间命名生成jpg文件），完成监听程序退出（封装成DLL，由C#调用，目前只能在程序退出时得到程序退出消息，还没拦截关闭消息，后面修改）

---
+++ LabShell/LabShell/ConfigWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Forms;
+
+namespace LabShell
+{
+    /// <summary>
+    /// ConfigWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class ConfigWindow : Window
+    {
+		private List<ListenPath> list = new List<ListenPath>();
+		
+        public ConfigWindow()
+        {
+            InitializeComponent();
+            this.DataBinding();
+        }
+
+        public void DataBinding() 
+        {
+            //for (int i = 0; i < 5; i++)
+            //{
+            //    ListenPath listenpath = new ListenPath()
+            //    {
+                    
+            //        Path = "C:\\" + i
+
+            //    };
+            //    this.list.Add(listenpath);
+            //}
+            this.listbox.ItemsSource = this.list;
+        }
+		
+		private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Controls.Button btn = sender as System.Windows.Controls.Button;
+            foreach (ListenPath lp in list)
+            {
+                string tag =  btn.Tag.ToString();
+                if (lp.Path == tag)
+                {
+                    this.list.Remove(lp);
+                    break;
+                }
+            }
+            this.listbox.Items.Refresh();
+            
+		}
+
+        private void BrowserButton_Click(object sender, RoutedEventArgs e)
+        {
+            FolderBrowserDialog m_Dialog = new FolderBrowserDialog();
+            DialogResult result = m_Dialog.ShowDialog();
+
+            if (result == System.Windows.Forms.DialogResult.Cancel)
+            {
+                return;
+            }
+            string m_Dir = m_Dialog.SelectedPath.Trim();
+            this.PathText.Text = m_Dir;
+        }
+
+        private void AddButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+            this.list.Add(new ListenPath() { Path=this.PathText.Text});
+            this.listbox.Items.Refresh();
+            this.PathText.Text = "";
+        }
+        
+    }
+	
+	public class ListenPath
+	{
+		string path;
+        
+		
+		public string Path
+		{
+			get{return path;}
+			set{path = value;}
+		}
+
+        
+	}
+}
