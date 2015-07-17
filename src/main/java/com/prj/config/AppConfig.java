0222684e50690c42d9e8a898dbdd61eccbbd4024
Liam Globit <guoylyy@gmail.com>
2015-07-17 15:59:03 +0800

    add cache

--- src/main/java/com/prj/config/AppConfig.java
+++ src/main/java/com/prj/config/AppConfig.java
@@ -10,11 +10,14 @@ import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.service.ServiceRegistry;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.ehcache.EhCacheCacheManager;
+import org.springframework.cache.ehcache.EhCacheManagerFactoryBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
@@ -53,6 +56,23 @@ public class AppConfig implements TransactionManagementConfigurer {
         ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder().applySettings(
             config.getProperties()).build();
         return config.buildSessionFactory(serviceRegistry);
+    }
+
+    /**
+     * 配置缓存
+     * @return
+     */
+    @Bean
+    public EhCacheCacheManager ehCacheCacheManager() {
+        return new EhCacheCacheManager(ehCacheFactoryBean().getObject());
+    }
+
+    @Bean
+    public EhCacheManagerFactoryBean ehCacheFactoryBean() {
+        EhCacheManagerFactoryBean cacheManager = new EhCacheManagerFactoryBean();
+        cacheManager.setConfigLocation(new ClassPathResource("ehcache.xml"));
+        cacheManager.setShared(true);
+        return cacheManager;
     }
 
     @Bean
